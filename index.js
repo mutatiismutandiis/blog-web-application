dbfbd6608de3cf669e53ba67d802eecfb719b432
Paula Cardone <paula.cardone@buhlergroup.com>
2024-02-09 17:34:51 +0100

    server connected

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static("public"));
+
+// Handler para la home page
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+  });
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
